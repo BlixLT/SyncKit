e580e0dfdd4738f3f2f5fa1c915c32affac95414
Martynas Limontas <martynas@jumsoft.com>
2020-07-15 11:03:29 +0300

    disable undo registration

--- SyncKit/Classes/CoreData/DefaultCoreDataAdapterDelegate.swift
+++ SyncKit/Classes/CoreData/DefaultCoreDataAdapterDelegate.swift
@@ -26,6 +26,9 @@ public class DefaultCoreDataAdapterDelegate: CoreDataAdapterDelegate {
     
     public func coreDataAdapter(_ adapter: CoreDataAdapter, didImportChanges importContext: NSManagedObjectContext, completion: (Error?) -> ()) {
         var saveError: Error?
+        adapter.targetContext.performAndWait {
+            adapter.targetContext.undoManager?.disableUndoRegistration()
+        }
         importContext.performAndWait {
             do {
                 try importContext.save()
@@ -36,11 +39,20 @@ public class DefaultCoreDataAdapterDelegate: CoreDataAdapterDelegate {
         
         if saveError == nil {
             adapter.targetContext.performAndWait {
+                adapter.targetContext.processPendingChanges()
+                adapter.targetContext.undoManager?.enableUndoRegistration()
                 do {
                     try adapter.targetContext.save()
                 } catch {
                     saveError = error
                 }
+            }
+        }
+        else
+        {
+            adapter.targetContext.performAndWait {
+                adapter.targetContext.processPendingChanges()
+                adapter.targetContext.undoManager?.enableUndoRegistration()
             }
         }
         completion(saveError)
